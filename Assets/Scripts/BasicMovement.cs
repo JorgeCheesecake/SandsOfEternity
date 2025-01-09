fd4c9f7a7c4be17a7e4b724d7633c7f728dac7e7
JorgeCheesecake <jorgeql21@educastur.es>
2025-01-09 13:16:32 +0100

    3ª persona, movimiento camara y sprint

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
     public float speed = 5.0f;
-    public float jumpForce = 5.0f;
-    private bool isGrounded = false;
-    private float horizontalaxis;
-    private float verticalaxis;
+    public CharacterController character;
+    public float turnsmoothtime = 0.1f;
+    float turnsmoothvelocity;
+    public Transform cam;
+    public float jumpSpeed = 5.0f;
+    public float gravity = 9.81f;
+    private Vector3 moveDirection = Vector3.zero;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        horizontalaxis = Input.GetAxis("horizontal");
-        verticalaxis = Input.GetAxis("vertical");
-        Vector3 move = new Vector3(horizontalaxis, 0, verticalaxis);
-        rb.velocity = new Vector3(move.x * speed * Time.deltaTime, rb.velocity.y, move.z * speed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 5f;
+        }
 
+        float horizontalinput = Input.GetAxisRaw("Horizontal");
+        float verticalinput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontalinput, 0f, verticalinput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnsmoothvelocity, turnsmoothtime);
+
+            Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            character.Move(movedir * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        character.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 0.1f);
     }
 }
