0e9faeadc4025151849089eb83c7802313e52e35
JorgeCheesecake <jorgeql21@educastur.es>
2025-03-05 23:28:08 +0100

    Commit Relleno

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;  // Necesario para cargar escenas
 
 public class ChangeScene : MonoBehaviour
 {
-    // Campo público para que puedas ingresar el nombre de la escena en el Inspector
+    // Campo pï¿½blico para que puedas ingresar el nombre de la escena en el Inspector
     public string sceneName;
 
-    // Este método cambiará a la escena que se haya especificado en el campo 'sceneName'
+    // Este mï¿½todo cambiarï¿½ a la escena que se haya especificado en el campo 'sceneName'
     public void ChangeToScene()
     {
         // Cambiar a la escena especificada
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName); 
     }
 }
