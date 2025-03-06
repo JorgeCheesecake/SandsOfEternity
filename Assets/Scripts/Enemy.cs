22cdc4a19eba3a027f8e90572c41f064c469785d
JorgeCheesecake <jorgeql21@educastur.es>
2025-03-06 11:15:53 +0100

    CommitInicial

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,7 +18,7 @@ public class Enemy : MonoBehaviour
     
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         ani = GetComponent<Animator>();
         target = GameObject.Find("Player");
     }
