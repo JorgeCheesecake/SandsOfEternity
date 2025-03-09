0f7582bc10cf2a878741e9e0de06b81fd03d08a8
JorgeCheesecake <jorgeql21@educastur.es>
2025-03-09 13:14:22 +0100

    Animaciones de muerte jugador y enemigo

--- Assets/Scripts/BarraVidaEnemigo.cs
+++ Assets/Scripts/BarraVidaEnemigo.cs
@@ -7,10 +7,27 @@ public class BarraVidaEnemigo : MonoBehaviour
 {
     public int vida = 100;
     public Slider BarraVida;
+    private bool colisionDetectada = false;
 
     // Update is called once per frame
     void Update()
     {
         BarraVida.value = vida;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("espada") && !colisionDetectada)
+        {
+            colisionDetectada = true;
+            vida -= 10;
+            if (vida < 0) vida = 0; // Evita que el valor de vida sea negativo
+            Invoke("RestablecerColision", 1f); // Restablece la colisión después de 2 segundos
+        }
+    }
+
+    void RestablecerColision()
+    {
+        colisionDetectada = false;
     }
 }
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,6 +1,7 @@
-﻿using System.Data.Common;
-using UnityEditor.VersionControl;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThirdPersonController : MonoBehaviour
 {
@@ -24,11 +25,15 @@ public class ThirdPersonController : MonoBehaviour
     bool inputCrouch;
     bool inputSprint;
 
+    public Slider Barravida;
+
     Animator animator;
     CharacterController cc;
 
     private float groundDetectionDelay = 0.1f; // retardo de 0.1 segundos
     private float lastGroundDetectionTime = 0f;
+
+    bool morir = false;
 
     void Start()
     {
@@ -37,161 +42,180 @@ public class ThirdPersonController : MonoBehaviour
 
         if (animator == null)
             Debug.LogWarning("Hey buddy, you don't have the Animator component in your player. Without it, the animations won't work.");
-
-        espada.SetActive(false);
     }
 
     void OnTriggerEnter(Collider coll)
     {
         if (coll.CompareTag("arma"))
         {
-            print("Daño");
+            Barravida.value -= 25;
         }
     }
 
     void Update()
     {
-        inputHorizontal = Input.GetAxis("Horizontal");
-        inputVertical = Input.GetAxis("Vertical");
-        inputJump = Input.GetAxis("Jump") == 1f;
-        inputSprint = Input.GetKey(KeyCode.LeftShift);
-
-        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.JoystickButton1))
-            isCrouching = true;
-        else
+        if (!morir)
         {
-            isCrouching = false;
-        }
+            inputHorizontal = Input.GetAxis("Horizontal");
+            inputVertical = Input.GetAxis("Vertical");
+            inputJump = Input.GetAxis("Jump") == 1f;
+            inputSprint = Input.GetKey(KeyCode.LeftShift);
 
-        if (inputSprint)
-            isSprinting = true;
-        else
-        {
-            isSprinting = false;
-        }
-
-        if (Time.time - lastGroundDetectionTime >= groundDetectionDelay)
-        {
-            lastGroundDetectionTime = Time.time;
-
-            if (cc.isGrounded && animator != null)
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.JoystickButton1))
+                isCrouching = true;
+            else
             {
-                animator.SetBool("crouch", isCrouching);
-                animator.SetBool("run", isSprinting);
+                isCrouching = false;
             }
-        }
 
-        if(Input.GetMouseButtonDown(0))
-        {
-            animator.SetBool("attack", true);
-        }
-        else
-        {
-            animator.SetBool("attack", false);
-        }
+            if (inputSprint)
+                isSprinting = true;
+            else
+            {
+                isSprinting = false;
+            }
 
-        if (inputHorizontal != 0 || inputVertical != 0)
-        {
-            animator.SetBool("walk", true);
-        }
-        else
-        {
-            animator.SetBool("walk", false);
-        }
+            if (Time.time - lastGroundDetectionTime >= groundDetectionDelay)
+            {
+                lastGroundDetectionTime = Time.time;
 
-        if (inputJump)
-        {
-            isJumping = true;
-            animator.SetBool("air", true);
-        }
-        else if (cc.isGrounded)
-        {
-            animator.SetBool("air", false);
-        }
+                if (cc.isGrounded && animator != null)
+                {
+                    animator.SetBool("crouch", isCrouching);
+                    animator.SetBool("run", isSprinting);
+                }
+            }
 
-        HeadHittingDetect();
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetBool("attack", true);
+            }
+            else
+            {
+                animator.SetBool("attack", false);
+            }
 
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            espada.SetActive(true);
-            animator.SetBool("arma", true);
+            if (inputHorizontal != 0 || inputVertical != 0)
+            {
+                animator.SetBool("walk", true);
+            }
+            else
+            {
+                animator.SetBool("walk", false);
+            }
+
+            if (inputJump)
+            {
+                isJumping = true;
+                animator.SetBool("air", true);
+            }
+            else if (cc.isGrounded)
+            {
+                animator.SetBool("air", false);
+            }
+
+            HeadHittingDetect();
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                espada.SetActive(true);
+                animator.SetBool("arma", true);
+            }
+            else
+            {
+                espada.SetActive(false);
+            }
+
+            if (Barravida.value < 0)
+            {
+                Barravida.value = 0;
+            }
+
+            if (Barravida.value == 0)
+            {
+                ani.SetBool("morir", true);
+                morir = true;
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        float velocityAdittion = 0;
-        if (isSprinting == true)
-            velocity = 5f;
-        else if (isSprinting == false)
-            velocity = 3f;
-
-        if (isCrouching == true)
-            velocity = 1f;
-        else if (isCrouching == false)
+        if (!morir)
         {
-            velocity = 3f;
-        }
+            float velocityAdittion = 0;
+            if (isSprinting == true)
+                velocity = 5f;
+            else if (isSprinting == false)
+                velocity = 3f;
 
-        if (isSprinting == true && isCrouching == true)
-        {
-            velocity = 2f;
-        }
-        else if (isSprinting == false && isCrouching == true)
-        {
-            velocity = 1f;
-        }
-        else if (isSprinting == true && isCrouching == false)
-        {
-            velocity = 5f;
-        }
-
-        float directionX = inputHorizontal * (velocity + velocityAdittion) * Time.deltaTime;
-        float directionZ = inputVertical * (velocity + velocityAdittion) * Time.deltaTime;
-        float directionY = 0;
-
-        if (isJumping)
-        {
-            directionY = Mathf.SmoothStep(jumpForce, jumpForce * 0.30f, jumpElapsedTime / jumpTime) * Time.deltaTime;
-
-            jumpElapsedTime += Time.deltaTime;
-            if (jumpElapsedTime >= jumpTime)
+            if (isCrouching == true)
+                velocity = 1f;
+            else if (isCrouching == false)
             {
-                isJumping = false;
-                jumpElapsedTime = 0;
+                velocity = 3f;
             }
-        }
 
-        directionY = directionY - gravity * Time.deltaTime;
+            if (isSprinting == true && isCrouching == true)
+            {
+                velocity = 2f;
+            }
+            else if (isSprinting == false && isCrouching == true)
+            {
+                velocity = 1f;
+            }
+            else if (isSprinting == true && isCrouching == false)
+            {
+                velocity = 5f;
+            }
 
-        Vector3 forward = Camera.main.transform.forward;
-        Vector3 right = Camera.main.transform.right;
+            float directionX = inputHorizontal * (velocity + velocityAdittion) * Time.deltaTime;
+            float directionZ = inputVertical * (velocity + velocityAdittion) * Time.deltaTime;
+            float directionY = 0;
 
-        forward.y = 0;
-        right.y = 0;
+            if (isJumping)
+            {
+                directionY = Mathf.SmoothStep(jumpForce, jumpForce * 0.30f, jumpElapsedTime / jumpTime) * Time.deltaTime;
 
-        forward.Normalize();
-        right.Normalize();
+                jumpElapsedTime += Time.deltaTime;
+                if (jumpElapsedTime >= jumpTime)
+                {
+                    isJumping = false;
+                    jumpElapsedTime = 0;
+                }
+            }
 
-        forward = forward * directionZ;
-        right = right * directionX;
+            directionY = directionY - gravity * Time.deltaTime;
 
-        if (directionX != 0 || directionZ != 0)
-        {
-            float angle = Mathf.Atan2(forward.x + right.x, forward.z + right.z) * Mathf.Rad2Deg;
-            Quaternion rotation = Quaternion.Euler(0, angle, 0);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
-        }
+            Vector3 forward = Camera.main.transform.forward;
+            Vector3 right = Camera.main.transform.right;
 
-        Vector3 verticalDirection = Vector3.up * directionY;
-        Vector3 horizontalDirection = forward + right;
+            forward.y = 0;
+            right.y = 0;
 
-        Vector3 moviment = verticalDirection + horizontalDirection;
-        cc.Move(moviment);
+            forward.Normalize();
+            right.Normalize();
 
-        if(Input.GetKey(KeyCode.E))
-        {
-            ani.SetBool("arma", true);
+            forward = forward * directionZ;
+            right = right * directionX;
+
+            if (directionX != 0 || directionZ != 0)
+            {
+                float angle = Mathf.Atan2(forward.x + right.x, forward.z + right.z) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.Euler(0, angle, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+
+            Vector3 verticalDirection = Vector3.up * directionY;
+            Vector3 horizontalDirection = forward + right;
+
+            Vector3 moviment = verticalDirection + horizontalDirection;
+            cc.Move(moviment);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                ani.SetBool("arma", true);
+            }
         }
     }
 
